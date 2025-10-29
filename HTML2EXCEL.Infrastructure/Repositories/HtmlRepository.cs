ea3b53743e94b0ca7a51e8069f3a007ba8289d0d
Fatima <jazaeifateme@gmail.com>
2025-10-29 14:29:37 +0330

    feat(infra): Add HtmlRepository

---
+++ HTML2EXCEL.Infrastructure/Repositories/HtmlRepository.cs
@@ -0,0 +1,30 @@
+﻿using HTML2EXCEL.Domain.Interfaces;
+using HTML2EXCEL.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTML2EXCEL.Infrastructure.Repositories
+{
+    public class HtmlRepository : IHtmlRepository
+    {
+        private readonly AppDbContext _context;
+
+        public HtmlRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<string?> GetHtmlContentAsync(int id)
+        {
+            var entity = await _context.HtmlContents
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return entity?.Html;
+        }
+    }
+}
