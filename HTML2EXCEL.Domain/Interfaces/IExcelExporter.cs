44f0a108e3e239edf939bd3724d997a6a8acb883
Fatima <jazaeifateme@gmail.com>
2025-11-02 12:01:03 +0330

    Fix: Export Excel successfully

--- HTML2EXCEL.Domain/Interfaces/IExcelExporter.cs
+++ HTML2EXCEL.Domain/Interfaces/IExcelExporter.cs
@@ -4,20 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace HTML2EXCEL.Domain.Interfaces
 {
     public interface IExcelExporter
     {
+        XLWorkbook GetWorkbook();
         Task<MemoryStream> CreateWorkbookAsync();
-        Task<int> WriteTextAsync(string text, int row);
-        Task<int> WriteTableAsync(List<List<string>> data, int startRow);
 
         /// <summary>
         /// Create an Excel file containing multiple tables.
         /// </summary>
         /// <param name="tables">List of TableData to export</param>
         /// <param name="outputPath">Full file path for Excel output</param>
+  
         Task SaveAsync(MemoryStream stream, string path);
     }
 }
--- HTML2EXCEL.Infrastructure/Services/ExcelExporter.cs
+++ HTML2EXCEL.Infrastructure/Services/ExcelExporter.cs
@@ -12,35 +12,17 @@ namespace HTML2EXCEL.Infrastructure.Services
     public class ExcelExporter : IExcelExporter
     {
         private readonly XLWorkbook _workbook;
-        private readonly IXLWorksheet _sheet;
 
         public ExcelExporter()
         {
             _workbook = new XLWorkbook();
-            _sheet = _workbook.Worksheets.Add("Report");
         }
+
+        public XLWorkbook GetWorkbook() => _workbook;
+
 
         public Task<MemoryStream> CreateWorkbookAsync()
             => Task.FromResult(new MemoryStream());
-
-        public async Task<int> WriteTextAsync(string text, int row)
-        {
-            _sheet.Cell(row, 1).Value = text;
-            _sheet.Cell(row, 1).Style.Font.Bold = false;
-            return await Task.FromResult(row + 1);
-        }
-
-        public async Task<int> WriteTableAsync(List<List<string>> data, int startRow)
-        {
-            int r = startRow;
-            foreach (var row in data)
-            {
-                for (int c = 0; c < row.Count; c++)
-                    _sheet.Cell(r, c + 1).Value = row[c];
-                r++;
-            }
-            return await Task.FromResult(r + 1);
-        }
 
         public async Task SaveAsync(MemoryStream stream, string path)
         {
